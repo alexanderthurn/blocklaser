00ba3d29451bd4ccd821a6023b025fe0ebb8d107
Alexander Thurn <alexander.thurn@frontendsolutions.de>
2024-11-06 19:33:19 +0100

    time

--- index.js
+++ index.js
@@ -24,6 +24,8 @@
             let c = new PIXI.Graphics()
             .rect(w*x,h*y, w,h)
             .stroke('white')
+            c.laser = 0
+            c.alpha = 0
             container.addChild(c)
         }
     }
@@ -31,21 +33,66 @@
    
     app.stage.addChild(container);
 
-
-    document.body.addEventListener('pointermove', event => {
-        let x = Math.floor(event.clientX / w) % xCount
-        let y = Math.floor(event.clientY / h) % yCount
+    var laserSpottedOn = (pixelX,pixelY) => {
+        let x = Math.floor(pixelX/ w) % xCount
+        let y = Math.floor(pixelY / h) % yCount
         let i = Math.floor(xCount*y + x)
         let c = container.children[i]
-        console.log(x,y,xCount, yCount, i,c)
-        c.alpha = 0
+        //c.alpha = 0
+        c.laser++
+         if (c.laser < 50) {
+            c.laser++
+        } 
+   
+    }
+
+    let laserPointers = []
+
+    document.body.addEventListener('pointermove', event => {
+        if (laserPointers.length === 0) {
+            laserPointers.push({x: 0,y:0})
+        }
+        laserPointers[0].x = event.clientX
+        laserPointers[0].y = event.clientY
         event.preventDefault();
         event.stopPropagation();
     }, false);
 
     app.ticker.add(() =>
     {
+        laserPointers.forEach(l => {
+            laserSpottedOn(l.x,l.y)
+        })
 
+        container.children.forEach(c => {
+            if (c.laser > 0 && c.laser < 50) {
+                c.laser -= 0.5
+                if (c.laser < 0) {
+                    c.laser = 0
+                }
+            }
+
+            if (c.laser > 100 && c.laser < 150) {
+                c.laser -= 0.5
+                if (c.laser < 100) {
+                    c.laser = 100
+                }
+            }
+
+
+            if (c.laser > 200) {
+                c.laser = 0
+            } else if (c.laser > 150) {
+                c.alpha = 0
+            } else if (c.laser > 100) {
+                c.alpha = 1.0 - (c.laser-100) / 50
+            } else if (c.laser > 50) {
+               c.alpha = 1
+            } else {
+                c.alpha = c.laser / 50
+            } 
+
+        })
         //container.rotation += 0.01;
     });
 })();
