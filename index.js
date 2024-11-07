8dcd9d57fa2d4f3badddbc46692c7eacf72660c9
Alexander Thurn <alexander.thurn@frontendsolutions.de>
2024-11-07 10:15:13 +0100

    delete button

--- index.js
+++ index.js
@@ -15,30 +15,59 @@
 
     let w = 50
     let h = 50
-    let xCount = Math.ceil(app.screen.width / w)
-    let yCount = Math.ceil(app.screen.width / h)
+    let xCount = Math.ceil((app.screen.width-20) / w)
+    let yCount = Math.ceil((app.screen.height-20) / h)
     let container = new PIXI.Container()
+    container.x = 10
+    container.y = 10
     
+    let clearAll = (c) => {
+        container.children.forEach(c => { c.laser = 0})
+        c.laser = 100
+    }
+
+
     for (let y = 0; y < yCount; y++) {
          for (let x = 0; x < xCount; x++) {
             let c = new PIXI.Graphics()
             .rect(-w/2,-h/2, w,h)
             .stroke('white')
+            c.laser = 0
+           
+ 
+
+            if (x === 0 && y === 0 ){
+                c = new PIXI.Graphics()
+                .rect(-w/2,-h/2, w,h)
+                .stroke('white')
+                .moveTo(-w/2,-h/2)
+                .lineTo(w/2,h/2)
+                .moveTo(-w/2,h/2)
+                .lineTo(w/2,-h/2)
+                .stroke('white')
+                c.deleteAction = clearAll
+                c.laser = 100
+            }
+
+            if ((x === 4 && y === 5) || (x === 5 && y === 4) || (x === 6 && y === 5)) {
+                c.laser = 100
+            }
             c.x = w*x + w/2
             c.y = h*y+ h/2
-            c.laser = 0
             c.alpha = 0
             c.rotation = 0
+
             container.addChild(c)
         }
     }
   
+
    
     app.stage.addChild(container);
 
     var laserSpottedOn = (pixelX,pixelY) => {
-        let x = Math.floor(pixelX/ w) % xCount
-        let y = Math.floor(pixelY / h) % yCount
+        let x = Math.floor((pixelX-container.x)/ w) % xCount
+        let y = Math.floor((pixelY-container.y) / h) % yCount
         let i = Math.floor(xCount*y + x)
         let c = container.children[i]
 
@@ -81,16 +110,19 @@
             }
 
 
-            if (c.laser > 200) {
+            if (c.laser >= 200) {
                 c.laser = -50
                 c.angle = 0
-            } else if (c.laser > 150) {
+                if (c.deleteAction) {
+                    c.deleteAction(c)
+                }
+            } else if (c.laser >= 150) {
                 c.alpha = 1.0 - (c.laser-150) / 50
                 c.angle = -((c.laser-150)/50-1)*4+Math.random()*((c.laser-150)/50-1)*8
-            } else if (c.laser > 100) {
+            } else if (c.laser >= 100) {
                 c.alpha = 1
                 c.angle = 0
-            } else if (c.laser > 50) {
+            } else if (c.laser >= 50) {
                c.alpha = c.laser / 100
                c.angle = -(c.laser/50-1)*4+Math.random()*(c.laser/50-1)*8
             } else {
